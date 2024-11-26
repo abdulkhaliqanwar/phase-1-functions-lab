397c412a47e15f451521708ebd4eb08f2059b5f2
abdulkhaliq anwar <abdulkhaliqanwar@student,moringaschool.com>
2024-11-26 12:26:16 +0300

    phase-1-functions-lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const headquarters = 42;
+    return Math.abs(pickupLocation - headquarters);
+  }
+
+function distanceFromHqInFeet(pickupLocation){
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    const blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled * 264;
+}
+function calculatesFarePrice(start, destination){
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet <= 400){
+        return 0;
+    }   else if(distanceInFeet > 400 && distanceInFeet <= 2000){
+        return (distanceInFeet - 400) * 0.02;
+    }   else if (distanceInFeet > 2000 && distanceInFeet <=2500){
+        return 25;
+    }   else{
+        return "cannot travel that far";
+    }
+}
